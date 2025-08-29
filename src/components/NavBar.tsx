7a730be50c6a3e854f692e9bd858bebd62ccb86c
Dharmin Nagar <dharminnagar@gmail.com>
2025-08-29 10:42:52 +0530

    feat: add NavBar component and ESLint cache file

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function NavBar() {
+  return (
+    <div className="flex justify-between items-center min-h-[8vh]">
+      <div className="text-4xl">NavBar</div>
+    </div>
+  );
+}
+
+export default NavBar;
